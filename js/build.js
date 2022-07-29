b759f793ccdc5b244fa2d3c6c1807e72bf73e96c
Nicholas Valbusa <squallstar@gmail.com>
2022-07-29 18:39:43 +0200

    tweaks for initialization

--- js/build.js
+++ js/build.js
@@ -1,5 +1,12 @@
-Fliplet.Widget.instance('dynamic-container', function(data, parentContext) {
-  console.log('Dynamic container initializing', this, data, 'with parent context', parentContext);
-}, {
-  supportsDynamicContext: true
+Fliplet().then(function() {
+  Fliplet.Widget.instance('dynamic-container', function(data, parentContext) {
+    var $el = $(this);
+
+    // TODO: fetch required data
+    console.log(this, 'Data', data, 'parent context', parentContext);
+
+    Fliplet.Widget.initializeChildren(this, { foo: 'bar'});
+  }, {
+    supportsDynamicContext: true
+  });
 });
